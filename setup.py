5d744fdf78352bf179fad8302cb51c986ac14d54
John Di Zhang <zhangdijohn@gmail.com>
2017-08-13 21:52:31 +1000

    scheduled sns, handlers, and api endpoint

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+# def read(fname):
+#     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "sns_boomerang",
+    version = "1.0.0",
+    author = "John Di Zhang",
+    author_email = "zhangdijohn@gmail.com",
+    description = ("a serverless setup of scheduled tasks"),
+    license = "MIT",
+    keywords = "scheduled sns_resource",
+    url = "http://packages.python.org/an_example_pypi_project",
+    #    packages=['an_example_pypi_project', 'tests'],
+    # long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
---
+++ sns_boomerang/handlers/ttl_enforcer.py
@@ -0,0 +1,5 @@
+from sns_boomerang.common.items import Job
+
+
+def flush():
+    Job.flush()
